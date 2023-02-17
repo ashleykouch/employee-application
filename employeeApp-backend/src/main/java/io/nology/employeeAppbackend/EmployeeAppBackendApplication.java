9b41432b85cf4add6ac1c280131d3da8698c00d2
ashleykouch <ashleykouch99@gmail.com>
2023-02-17 15:15:19 +1100

    added sprint initialiser

---
+++ employeeApp-backend/src/main/java/io/nology/employeeAppbackend/EmployeeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package io.nology.employeeAppbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppBackendApplication.class, args);
+	}
+
+}
