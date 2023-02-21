b6d21901f90ebae16a29befd5aaa275cc6b84edb
ashleykouch <ashleykouch99@gmail.com>
2023-02-21 14:40:47 +1100

    finalised styling on home page

--- employeeApp/src/components/HomePage/HomePage.tsx
+++ employeeApp/src/components/HomePage/HomePage.tsx
@@ -6,12 +6,21 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className="home">
-      <Link to="/employees">View employees</Link>
       <div className="home_img">
         <img src={HomeImg} alt="HomeImg" />
       </div>
       <div className="home_content">
-        <p>Your simple employee management application</p>
+        <h1 className="home_content_text">
+          YOUR SIMPLE EMPLOYEE MANAGEMENT APPLICATION
+        </h1>
+      </div>
+      <div className="home_btn">
+        <Link to="/employees">
+          <button className="home_btn_view">View employees</button>
+        </Link>
+        <Link to="/employee-details">
+          <button className="home_btn_add">Add Employees</button>
+        </Link>
       </div>
     </div>
   );
